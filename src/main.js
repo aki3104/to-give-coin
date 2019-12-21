71ff3200bfb80eab1c6b9458a50325dfdbc26578
Satoshi <aki319809@gmail.com>
2019-12-22 00:59:38 +0900

    add plugins Incomplete’
    q
    exit
    
    q

--- src/main.js
+++ src/main.js
@@ -4,25 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import firebase from 'firebase'
+// import firebase from 'firebase'
+import { plugins } from './plugins/firebase'
 
 Vue.config.productionTip = false
-
-// Your web app's Firebase configuration
-export var firebaseConfig = {
-  apiKey: process.env.fireBase.apiKey,
-  authDomain: process.env.fireBase.authDomain,
-  databaseURL: process.env.fireBase.databaseURL,
-  projectId: process.env.fireBase.projectId,
-  storageBucket: process.env.fireBase.storageBucket,
-  messagingSenderId: process.env.fireBase.messagingSenderId,
-  appId: process.env.fireBase.appId,
-  measurementId: process.env.fireBase.measurementId
-}
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
 
 /* eslint-disable no-new */
 
@@ -30,6 +15,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  plugins,
   components: { App },
   template: '<App/>'
 })
