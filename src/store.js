8ceb5c3b3cc1291f8c64a2897bd8896fca48fd44
Satoshi <aki319809@gmail.com>
2019-12-31 16:42:48 +0900

    fix firebase

--- src/store.js
+++ src/store.js
@@ -12,6 +12,12 @@ export default new Vuex.Store({
       firebase.auth().onAuthStateChanged(user => {
         if (user) {
           state.loginUser = user
+          let db = firebase.firestore()
+          db.collection(`users/${state.loginUser}/user`).add({
+            first: 'Ada',
+            last: 'Lovelace',
+            born: 1815
+          })
         }
       })
     },
@@ -20,12 +26,6 @@ export default new Vuex.Store({
       state.loginUser = ''
     },
     adduser (state) {
-    //   let db = firebase.firestore()
-    //       db.collection(`users/${state.loginUser}/user`).add({
-    //         first: 'Ada',
-    //         last: 'Lovelace',
-    //         born: 1815
-    //       })
     }
   },
   actions: {
